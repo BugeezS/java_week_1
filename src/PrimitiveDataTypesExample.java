e61293dd26250c89a00fb923342646f0502e5ff7
Bruno <brunolb.lafont@gmail.com>
2023-06-26 10:33:14 +0200

    add structure

---
+++ src/PrimitiveDataTypesExample.java
@@ -0,0 +1,35 @@
+public class PrimitiveDataTypesExample {
+    public static void main(String[] args) {
+        // Boolean
+        boolean isTrue = true;
+        System.out.println("Boolean example: " + isTrue);
+
+        // Byte
+        byte myByte = 127;
+        System.out.println("Byte example: " + myByte);
+
+        // Short
+        short myShort = 32000;
+        System.out.println("Short example: " + myShort);
+
+        // Int
+        int myInt = 1234567890;
+        System.out.println("Int example: " + myInt);
+
+        // Long
+        long myLong = 123456789012345L;
+        System.out.println("Long example: " + myLong);
+
+        // Float
+        float myFloat = 3.14f;
+        System.out.println("Float example: " + myFloat);
+
+        // Double
+        double myDouble = 3.141592653589793;
+        System.out.println("Double example: " + myDouble);
+
+        // Char
+        char myChar = 'A';
+        System.out.println("Char example: " + myChar);
+    }
+}
